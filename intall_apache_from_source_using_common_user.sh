e5602519e44cfecb7ab34962867498ffcc71e2ad
liupeng <itsliupeng@gmail.com>
2015-04-07 11:24:37 +0800

    edit error

--- intall_apache_from_source_using_common_user.sh
+++ intall_apache_from_source_using_common_user.sh
@@ -23,8 +23,8 @@ echo "move apr and apr-util to httpd-2.4.12.tar.bz2/srclib/"
 mdkir ~/app_source/httpd-2.4.12.tar.bz2/srclib/apr
 mdkir ~/app_source/httpd-2.4.12.tar.bz2/srclib/apr-util
 
-mv ~/app_source/apr-1.5.1/* ~/app_source/httpd-2.4.12.tar.bz2/srclib/apr/
-mv ~/app_source/apr-util-1.5.1/* ~/app_source/httpd-2.4.12.tar.bz2/srclib/apr-util/
+mv ~/app_source/apr-1.5.1/* ~/app_source/httpd-2.4.12/srclib/apr/
+mv ~/app_source/apr-util-1.5.1/* ~/app_source/httpd-2.4.12/srclib/apr-util/
 
 echo "make pcre"
 cd  ~/app_source/pcre-8.36
